99a1281be919d23b48995896919671046700093a
David <david.crittin@gmail.com>
2021-05-16 11:58:46 +0200

    Exercices exploration de graphe

--- ListesChainees/src/main/java/ch/hevs/students/david/exercices/ExplorationGraphe.java
+++ ListesChainees/src/main/java/ch/hevs/students/david/exercices/ExplorationGraphe.java
@@ -1,17 +1,29 @@
 package ch.hevs.students.david.exercices;
 
+import ch.hevs.students.david.lib.File;
 import ch.hevs.students.david.lib.Graphe;
 
 public class ExplorationGraphe {
     public static void main(String[] args) {
-        int matrice[][] = {{1, 2},
-                {4},
-                {1,3,5},
-                {5},
-                {3,5},
-                {}};
+        int[][] matrice = {
+                /* depuis 0 on peut aller à */ {1, 2, 3},
+                /* depuis 1 on peut aller à */ {4},
+                /* depuis 2 on peut aller à */ {4, 5, 6},
+                /* depuis 3 on peut aller à */ {6},
+                /* depuis 4 on peut aller à */ {7},
+                /* depuis 5 on peut aller à */ {7},
+                /* depuis 6 on peut aller à */ {7},
+                /* depuis 7 on peut aller à */ {},
+        };
 
         Graphe g = new Graphe(matrice);
         g.afficheListe();
+        //g.explorationRecursive(0);
+        g.explorationFile(0);
+        g.affichePredecesseurs();
+        File f1 = g.chemin(0, 7);
+        f1.affiche();
+        File f2 = g.chemin(2, 6);
+        f2.affiche();
     }
 }
--- ListesChainees/src/main/java/ch/hevs/students/david/lib/Graphe.java
+++ ListesChainees/src/main/java/ch/hevs/students/david/lib/Graphe.java
@@ -2,9 +2,9 @@ package ch.hevs.students.david.lib;
 
 public class Graphe {
     private final File[] liste;
+    private int[] predecesseurs;
 
     public Graphe() {
-
         liste = new File[0];
     }
 
@@ -16,6 +16,11 @@ public class Graphe {
 
     public Graphe(int matrice[][]) {
         int n = matrice.length;
+        predecesseurs = new int[n];
+        for (int i = 0; i < n; i++) {
+            predecesseurs[i] = 0;
+        }
+        //affichePredecesseurs();
 
         liste = new File[n];
         for (int i = 0; i < n; i++) {
@@ -33,7 +38,85 @@ public class Graphe {
         for (int i = 0; i < liste.length; i++) {
             System.out.print("file[" + i + "]: ");
             liste[i].affiche();
-            System.out.println();
+            //System.out.println();
         }
+    }
+
+    /**
+     * Dans votre classe Graphe écrivez une méthode d’exploration récursive, respectant l’idée suivante :
+     * Si le sommet est marqué on arrête.
+     * Sinon, on le marque et on lance l’exploration depuis l’ensemble de ses successeurs.
+     * On affichera les sommets au fur et à mesure du marquage
+     */
+    public void explorationRecursive(int sommet) {
+        boolean[] sommetsVisites = new boolean[liste.length];
+        _explorationRecursive(sommet, sommetsVisites);
+    }
+
+    public void _explorationRecursive(int sommet, boolean[] sommetsVisites) {
+        // clause de finitude
+        if(sommetsVisites[sommet])
+            return;
+
+        // pas récursif
+        System.out.println("Marquage de : " + sommet);
+        sommetsVisites[sommet] = true;
+
+        while(liste[sommet].getPremier() != null) {
+            Noeud current = liste[sommet].defile();
+
+            if(predecesseurs[current.getInfo().getValeur()] == 0) {
+                predecesseurs[current.getInfo().getValeur()] = sommet;
+            }
+
+            _explorationRecursive(current.getInfo().getValeur(), sommetsVisites);
+        }
+    }
+
+    public void explorationFile(int depart) {
+        boolean[] sommetsVisites = new boolean[liste.length];
+        File f = new File();
+        f.enfile(new Noeud(new Info(depart)));
+
+        while (!f.estVide()) {
+            int sommet = f.defile().getInfo().getValeur();
+            sommetsVisites[sommet] = true;
+            System.out.println("Marquage de : " + sommet);
+
+            Noeud courant = liste[sommet].getPremier();
+            while (courant != null) {
+                int suivant = courant.getInfo().getValeur();
+                if(predecesseurs[suivant] == 0)
+                    predecesseurs[suivant] = sommet;
+
+                if(!sommetsVisites[suivant]) {
+                    if(!f.contient(suivant)) {
+                        f.enfile(new Noeud(new Info(courant.getInfo().getValeur())));
+                    }
+                }
+                courant = courant.getSuivant();
+            }
+        }
+    }
+
+    public void affichePredecesseurs() {
+        for (int predecesseur : predecesseurs) {
+            System.out.print(predecesseur + " ");
+        }
+        System.out.println();
+    }
+
+    public File chemin(int debut, int fin) {
+        File f = new File();
+        f.enfile(new Noeud(new Info(fin)));
+
+        while (fin != 0) {
+            f.enfile(new Noeud(new Info(predecesseurs[fin])));
+            fin = predecesseurs[fin];
+        }
+
+        //_chemin(debut, fin, f);
+        f.inverse();
+        return f;
     }
 }
